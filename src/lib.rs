f505062c3ead6042a5d9c77b33d63d07ae229cdc
Stephen Morgan <steve@rebelops.io>
2023-11-12 16:39:06 -0500

    fix: update logic, tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,27 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use zero2prod::run;
 
 #[actix_web::main]
-async fn main() -> Result<(), std::io::Error> {
-    run().await
+async fn main() -> std::io::Result<()> {
+    let listener =
+        std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
 }
